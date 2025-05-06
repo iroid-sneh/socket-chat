fd66f3508a1718d6f292d9bd7099ae3ab9c17f1e
Your GitHub Username <Your GitHub Email>
2025-05-06 11:49:55 +0530

    fix: image not showing on live server on render fixed.

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -19,7 +19,7 @@ class authServices {
                 });
             }
 
-            console.log("FIle", file);
+            // console.log("FIle", file);
             const hashedPassword = await bcrypt.hash(password, 10);
             const image = `users/${file.filename}`;
             const user = await User.create({
---
+++ src/common/config/cloudinary.js
@@ -0,0 +1,5 @@
+import cloudinary, { v2 } from "cloudinary";
+import CloudinaryStorage from "multer-storage-cloudinary";
+
+
+
